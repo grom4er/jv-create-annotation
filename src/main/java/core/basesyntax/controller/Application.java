e9bd86f045119baac63252fb3844adeed7b8ab8c
Tonitrus <grom4er@gmail.com>
2021-01-08 11:55:22 +0200

    Fix at start Application add more information

--- src/main/java/core/basesyntax/controller/Application.java
+++ src/main/java/core/basesyntax/controller/Application.java
@@ -5,6 +5,7 @@ import core.basesyntax.controller.controller.ConsoleHandler;
 public class Application {
     public static void main(String[] args) {
         ConsoleHandler handler = new ConsoleHandler();
+        System.out.println("Enter please risk and you bet");
         handler.handle();
     }
 }
